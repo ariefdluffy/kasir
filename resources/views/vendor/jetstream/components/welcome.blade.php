220af64d3384669698e92f26300322f5066f19b6
arief d. Luffy <arieftheluffy@gmail.com>
2020-10-08 12:48:27 +0800

    update

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -4,14 +4,11 @@
     </div>
 
     <div class="mt-8 text-2xl">
-        Welcome KasirOnline!
+        Transaksi
     </div>
 
     <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+        Transaksi disini.
     </div>
 </div>
 
@@ -24,7 +21,7 @@
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
-                Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+                Transaksi
             </div>
 
             <a href="https://laravel.com/docs">
